4ac058319de97337408acad80c87c904feb77698
Steve Lyons <steve@stevelyons.com>
2015-02-02 23:59:43 -0600

    HelloPrinter.java

---
+++ HelloPrinter.java
@@ -0,0 +1,9 @@
+// first program from Java Book
+public class HelloPrinter 
+{
+	public static void main(String[] args)
+	{
+		// Display a greeting in the console window
+		System.out.println("Hello, World!");
+	}
+}
