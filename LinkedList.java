a98ef15e747703bded5e8bcb328e13bae1fab2f4
Steve Lyons <steve@stevelyons.com>
2015-02-26 08:20:54 -0600

    Create LinkedList.java
    Simple LinkedList with basic operations

---
+++ LinkedList.java
@@ -0,0 +1,18 @@
+package com.java2novice.linkedlist;
+import java.util.LinkedList;
+
+public class LinkedList 
+{ 
+  public static void main(String a[])
+  { 
+    LinkedList<String> ll = new LinkedList<String>();
+    ll.add("Orange");
+    ll.add("Apple");
+    ll.add("Grape");
+    ll.add("Banana"); 
+    System.out.println(ll); 
+    System.out.println("Size of the linked list: "+ll.size());
+    System.out.println("Is LinkedList empty? "+ll.isEmpty()); 
+    System.out.println("Does LinkedList contains 'Grape'? "+ll.contains("Grape")); 
+    } 
+} 
