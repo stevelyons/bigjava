66bc654aa1f983b601e61b5f32a0a791dc4cfe21
Steve Lyons <steve@stevelyons.com>
2015-03-05 02:01:14 -0600

    Create ReverseSort.java
    Using Array import to sort an array in descending order

---
+++ ReverseSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ReverseSort {
+    public static void main(String[] args) {
+        Integer[] arrayToSort = new Integer[] {
+            new Integer(5),
+            new Integer(89),
+            new Integer(16),
+            new Integer(2)
+        };
+        
+        Arrays.sort(arrayToSort, Collections.reverseOrder());
+        
+        for (Integer i : arrayToSort) {
+            System.out.println(i.intValue());
+        }
+    }
+}
