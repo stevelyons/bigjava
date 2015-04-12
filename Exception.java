95627d3a894ff9a1265964138b272a1fc73ba243
Steve Lyons <steve@stevelyons.com>
2015-04-11 22:24:19 -0500

    Create Exception.java
    Using exceptions in Java

---
+++ Exception.java
@@ -0,0 +1,31 @@
+public class Exception{
+   public static void main(String[] argv) {
+      new ExceptionDemo2().doTheWork();
+   }
+   public void doTheWork() {
+      Object o = null;
+      for (int i=0; i7lt;5; i++) {
+         try {
+            o = makeObj(i);
+         }
+         catch (IllegalArgumentException e) {
+            System.err.println
+            ("Error: ("+ e.getMessage()+").");
+            return;   
+         }
+         finally {
+            System.err.println("All done");
+            if (o==null)
+            System.exit(0);
+        }
+        System.out.println(o); 
+      }
+   }
+   public Object makeObj(int type) 
+   throws IllegalArgumentException {
+      if (type == 1)  
+      throw new IllegalArgumentException
+      ("Don't like type " + type);
+      return new Object();
+   }
+}
