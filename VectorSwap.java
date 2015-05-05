7c59801f54134c5c42839abbf235a20ff9c33500
Steve Lyons <steve@stevelyons.com>
2015-05-05 18:18:05 -0500

    Create VectorSwap.java
    Swapping numbers in a vector

---
+++ VectorSwap.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.Vector;
+
+public class VectorSwap 
+{
+   public static void main(String[] args) 
+   {
+      Vector<String> v = new Vector();
+      v.add("1");
+      v.add("2");
+      v.add("3");
+      v.add("4");
+      v.add("5");
+      System.out.println(v);
+      Collections.swap(v, 0, 4);
+      System.out.println("After swapping");
+      System.out.println(v);
+   }
+}
